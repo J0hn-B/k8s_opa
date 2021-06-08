6275edd4ec0923548e8726d4998be485fd19add5
J0hn-B <v-jbenakis@microsoft.com>
2021-06-08 18:39:47 +0100

    update folder structure

---
+++ tests/scripts/opa-values-generator.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+set -e
+
+#
+# Shell Script
+# - OPA Run Tests
+#
+# # Parameters
+PLAN_NAME=terraform-plan
+VERSION=v4.9.3
+BINARY=yq_linux_amd64
+
+if [ $(command -v terraform) ]; then
+    echo "==> Terraform exists, skip install"
+    terraform version
+    echo
+else
+    echo "==> Install Terraform on Linux..."
+    sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+    sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+    sudo apt-get update && sudo apt-get install terraform
+fi
+
+if [ $(command -v jq) ]; then
+    echo "==> jq exists, skip install"
+    jq --version
+    echo
+else
+    echo "==> Install jq on Linux..."
+    sudo apt-get install jq
+fi
+
+if [ $(command -v yq) ]; then
+    echo "==> yq exists, skip install"
+    yq --version
+    echo
+else
+    echo "==> Install yq on Linux..."
+    wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY} -O /usr/bin/yq &&
+        chmod +x /usr/bin/yq
+fi
+
+echo "==> Change to the module root directory..."
+cd ../tests/deployment
+
+echo "==> Initializing infrastructure..."
+terraform init
+
+echo "==> Planning infrastructure..."
+terraform plan --out $PLAN_NAME
+
+echo "==> Converting plan to *.json..."
+terraform show -json $PLAN_NAME >$PLAN_NAME.json
+
+echo "==> Removing the original plan..."
+rm $PLAN_NAME
+
+echo "==> Saving planned values to a temporary planned_values.json..."
+cat $PLAN_NAME.json | jq '.planned_values.root_module' >planned_values.json
+
+echo "==> Converting to yaml..."
+cat planned_values.json | yq e -P - | tee ../../opa/policy/planned_values_template.yml
+
+echo "==> Removing the temporary planned_values.json..."
+rm planned_values.json
+
+echo "==> Remove terraform-plan.json?"
+read -p "Do you want to remove terraform-plan.json (y/n)?" CONT
+if [ "$CONT" = "y" ]; then
+    rm $PLAN_NAME.json
+    echo
+    echo "$PLAN_NAME.json has been removed from your root module"
+    echo
+else
+    echo
+    echo "$PLAN_NAME.json  can contain sensitive data"
+    echo
+    echo "Exposing $PLAN_NAME.json in a repository can cause security breach"
+fi
