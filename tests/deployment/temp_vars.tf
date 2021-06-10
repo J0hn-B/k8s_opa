cdd3098ed88c61cca34ae11c664071a3e5459be6
J0hn-B <v-jbenakis@microsoft.com>
2021-06-10 11:51:38 +0100

    remove planned_values

--- tests/deployment/temp_vars.tf
+++ tests/deployment/temp_vars.tf
@@ -1,12 +1,12 @@
 # temp test vars
 
 
-# variable "root_id_1" { default = "myorg-1" }
+variable "root_id_1" { default = "myorg-1" }
 
-# variable "root_id_2" { default = "myorg-2" }
+variable "root_id_2" { default = "myorg-2" }
 
-# variable "root_id_3" { default = "myorg-3" }
+variable "root_id_3" { default = "myorg-3" }
 
-# variable "root_name" { default = "My Organization" }
+variable "root_name" { default = "My Organization" }
 
-# variable "location" { default = "uksouth" }
+variable "location" { default = "uksouth" }
