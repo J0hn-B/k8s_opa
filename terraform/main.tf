df4cdc67478f7ad2c43123476977979038fde8fe
J0hn-B <connect-wsman@hotmail.com>
2021-07-02 20:15:47 +0100

    update dir

--- terraform/main.tf
+++
@@ -1,9 +0,0 @@
-
-# # # ? Create a k3d cluster
-resource "null_resource" "create_k3d_cluster" {
-  provisioner "local-exec" {
-    command     = "${path.module}./linux/k3d_prepare.sh"
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
-
