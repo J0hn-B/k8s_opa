be41fe799bb19426aae88ea04d9ef5b981d71813
J0hn-B <connect-wsman@hotmail.com>
2021-07-03 18:59:45 +0100

    comment out docker rm volumes

--- linux/k3d_cleanup.sh
+++ linux/k3d_cleanup.sh
@@ -3,8 +3,8 @@
 echo "==> K3d cluster is deleted"
 k3d cluster delete dev-cluster
 
-echo "==> Delete all docker containers"
-docker rm -f "$(docker ps -a -q)"
+# echo "==> Delete all docker containers"
+# docker rm -f "$(docker ps -a -q)"
 
-echo "==> Delete all docker volumes"
-docker volume rm "$(docker volume ls -q)"
+# echo "==> Delete all docker volumes"
+# docker volume rm "$(docker volume ls -q)"
