98d6cb8657921ecf4271ce099ec15f65492294b3
J0hn-B <connect-wsman@hotmail.com>
2021-07-06 18:29:27 +0100

    update linter rules

--- tests/validate_code.sh
+++ tests/validate_code.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Lint code before push
-docker run --rm -e RUN_LOCAL=true -v /home/johnb/temp/opa:/tmp/lint ghcr.io/github/super-linter:slim-v4
+docker run --rm -e KUBERNETES_KUBEVAL_OPTIONS=--ignore-missing-schemas -e RUN_LOCAL=true -v /home/johnb/temp/opa:/tmp/lint ghcr.io/github/super-linter:slim-v4
