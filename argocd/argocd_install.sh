5ed6c3efd6439d721e06c5e2c7eb63e07bd81631
J0hn-B <connect-wsman@hotmail.com>
2021-07-06 17:01:43 +0100

    add argocd app yaml config

--- argocd/argocd_install.sh
+++ argocd/argocd_install.sh
@@ -5,6 +5,16 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 # Parameters
+ARGOCD_CLI_VERSION=$(argocd version)
+
+# Install Argo CD CLI
+if [ "$ARGOCD_CLI_VERSION" ]; then
+    echo -e "${GREEN}==> Argo CD:${NC} $ARGOCD_CLI_VERSION"
+else
+    echo -e "==> Installing Argo CD..."
+    sudo curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+    sudo chmod +x /usr/local/bin/argocd
+fi
 
 echo -e "==> Create argocd namespace..."
 kubectl create namespace argocd
@@ -22,3 +32,16 @@ while true; do
     kubectl port-forward svc/argocd-server -n argocd 8080:443
     sleep 3
 done
+
+### Login to argocd CLI
+#
+# argocd login --insecure localhost:8080
+#
+# argocd cluster add k3d-dev-cluster
+#
+#  argocd app list
+#
+# argocd app manifests opa
+#
+#
+#
